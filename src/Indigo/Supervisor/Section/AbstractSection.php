1013e25bc3a62d6cf5ead331b36e3ab7d07846b3
Sági-Kazár Márk <webmaster@firstcomputer.hu>
2013-12-15 18:10:44 +0100

    Comments added to AbstractSection

--- src/Indigo/Supervisor/Section/AbstractSection.php
+++ src/Indigo/Supervisor/Section/AbstractSection.php
@@ -6,10 +6,25 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class AbstractSection implements SectionInterface
 {
+	/**
+	 * Options
+	 *
+	 * @var array
+	 */
 	protected $options = array();
 
+	/**
+	 * Name of section (eg. supervisord or program:test)
+	 *
+	 * @var string
+	 */
 	protected $name;
 
+	/**
+	 * Create a new Section instance
+	 *
+	 * @param array $options
+	 */
 	public function __construct(array $options = array())
 	{
         $resolver = new OptionsResolver();
@@ -18,11 +33,21 @@ class AbstractSection implements SectionInterface
         $this->options = $resolver->resolve($options);
 	}
 
+	/**
+	 * Get the name of section
+	 *
+	 * @return string
+	 */
 	public function getName()
 	{
 		return $this->name;
 	}
 
+	/**
+	 * Get options
+	 *
+	 * @return array
+	 */
 	public function getOptions()
 	{
 		return $this->options;
