50d1acc1b0e1fe79f48d0e65d45a0a5c322ad207
Sági-Kazár Márk <webmaster@firstcomputer.hu>
2013-12-16 10:11:48 +0100

    Memmon ready for testing

--- src/Indigo/Supervisor/EventListener/MemmonEventListener.php
+++ src/Indigo/Supervisor/EventListener/MemmonEventListener.php
@@ -33,17 +33,37 @@ class MemmonEventListener extends AbstractEventListener
 		foreach ($processes as $process) {
 			$mem = $process->getMemUsage();
 
-			if (array_key_exists($process['name'], $this->programs) and $this->programs[$process['name']] >= $mem) {
-				$process->restart();
+			if ($maxMem = $this->isListeningTo(array($process['name'], $pname), $this->programs) and $mem > $maxMem) {
+				$this->restart($process, $mem);
+			} elseif ($maxMem = $this->isListeningTo($process['group'], $this->groups) and $mem > $maxMem) {
+				$this->restart($process, $mem);
+			} elseif ($mem > intval($this->any)) {
+				$this->restart($process, $mem);
 			}
 		}
+
+		return 0;
 	}
 
-	protected function restartProcess($process, $mem)
+	protected function restart($process, $mem)
 	{
-		$pname = $process['name'] . ':' . $process['group'];
+		$process->restart();
+	}
 
-		$this->supervisor->stopProcess($pname);
-		$this->supervisor->startProcess($pname);
+	protected function isListeningTo($what, $to = array())
+	{
+		if (is_array($what)) {
+			foreach ($what as $w) {
+				if (array_key_exists($w, $to)) {
+					return $to[$w];
+				}
+			}
+		} else {
+			if (array_key_exists($what, $to)) {
+				return $to[$what];
+			}
+		}
+
+		return false;
 	}
 }
