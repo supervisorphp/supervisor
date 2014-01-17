b4a87c40fff95c371fb8eb52d585fecb4d9376d9
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-01-17 16:09:50 +0100

    Correct port option

--- src/Indigo/Supervisor/Section/InetHttpServerSection.php
+++ src/Indigo/Supervisor/Section/InetHttpServerSection.php
@@ -2,16 +2,35 @@
 
 namespace Indigo\Supervisor\Section;
 
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\OptionsResolver\Options;
+
 class InetHttpServerSection extends AbstractSection
 {
     protected $name = 'inet_http_server';
 
     protected $requiredOptions = array(
-        'port' => 'string',
+        'port' => array('string', 'integer'),
     );
 
     protected $optionalOptions = array(
         'username' => 'string',
         'password' => 'string',
     );
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        parent::setDefaultOptions($resolver);
+
+        $resolver->setNormalizers(array(
+            'port' => function (Options $options, $value) {
+                is_int($value) and $value = '*:' . $value;
+
+                return $value;
+            },
+        ));
+    }
 }
