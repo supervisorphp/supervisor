28236521b4db3128bd3f80ad33dda320ba370814
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-01-18 16:30:41 +0100

    Added EventListener commands

---
+++ src/Indigo/Supervisor/Command/EventListener/MemmonEventListenerCommand.php
@@ -0,0 +1,82 @@
+<?php
+
+/*
+ * This file is part of the Indigo Supervisor package.
+ *
+ * (c) IndigoPHP Development Team
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Indigo\Supervisor\Command\EventListener;
+
+use Indigo\Supervisor\Command\AbstractCommand;
+use Indigo\Supervisor\EventListener\MemmonEventListener;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Memmon EventListener Command
+ *
+ * @author Márk Sági-Kazár <mark.sagikazar@gmail.com>
+ */
+class MemmonEventListenerCommand extends AbstractCommand
+{
+    protected $options = array(
+        array(
+            'program',
+            null,
+            self::VALUE_OPTIONAL_ARRAY,
+            'Program memory limits (program:limit)'
+        ),
+        array(
+            'group',
+            null,
+            self::VALUE_OPTIONAL_ARRAY,
+            'Group memory limits (group:limit)'
+        ),
+        array(
+            'any',
+            null,
+            InputOption::VALUE_OPTIONAL,
+            'Any memory limit'
+        ),
+    );
+
+    protected function configure()
+    {
+        $this
+            ->setName('event:memmon')
+            ->setDescription('Start a Memmon EventListener');
+
+        parent::configure();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $program = $input->getOption('program');
+        $group   = $input->getOption('group');
+        $any     = $input->getOption('any');
+
+        if (empty($program) and empty($group) and empty($any)) {
+            throw new \Exception('You have to specify at least on program, group, or any option.');
+        }
+
+        $program = $this->parseOption($program);
+        $group = $this->parseOption($group);
+
+        $listener = new MemmonEventListener($this->supervisor, $program, $group, $any);
+
+        $listener->listen();
+    }
+
+    private function parseOption(array $option)
+    {
+        return array_map(function ($item) {
+            return explode(':', $item);
+        }, $option);
+    }
+}
