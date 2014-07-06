8942057dc31e2be3075e7629aaf80e8f1ff9b467
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-07-06 19:29:47 +0200

    Adds supervisor installer script

---
+++ scripts/supervisor.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd /tmp
+wget https://pypi.python.org/packages/source/s/supervisor/supervisor-3.0.tar.gz
+tar -xfz supervisor-3.0.tar.gz
+cd supervisor-3.0
+sudo python setup.py install
+sudo mkdir -p /var/log/supervisor/
