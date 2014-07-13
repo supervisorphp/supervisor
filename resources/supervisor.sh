3cad69189b48ed90bb6b28b93a6d017c39e2f65a
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-07-13 10:18:19 +0200

    Resource changes

--- resources/supervisor.sh
+++ resources/supervisor.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 cd /tmp
+
 wget https://pypi.python.org/packages/source/s/supervisor/supervisor-3.0.tar.gz
 tar xfz supervisor-3.0.tar.gz
 cd supervisor-3.0
+
 sudo python setup.py install
