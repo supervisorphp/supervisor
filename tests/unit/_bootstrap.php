2950123bce0067cf0db9dd884d90a3f2d1ab604b
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-07-18 16:21:00 +0200

    Moves stubs to unit suite

--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -1,6 +1,10 @@
 <?php
 // Here you can initialize variables that will for your tests
 
+require_once __DIR__.'/stubs/DummyConnector.php';
+require_once __DIR__.'/stubs/DummySection.php';
+require_once __DIR__.'/stubs/DummyXmlrpcConnector.php';
+
 $GLOBALS['host'] = 'localhost:9001';
 $GLOBALS['username'] = 'user';
 $GLOBALS['password'] = 123;
