48fac20bf8c6eb2cd3e9e3c42299fc02184aca80
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-10-07 22:16:40 +0200

    Removes functional testing

--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -3,8 +3,3 @@
 
 require_once __DIR__.'/stubs/Connectors.php';
 require_once __DIR__.'/stubs/Sections.php';
-
-$GLOBALS['host'] = 'localhost:9001';
-$GLOBALS['username'] = 'user';
-$GLOBALS['password'] = 123;
-$GLOBALS['socket'] = '/var/run/supervisor.sock';
