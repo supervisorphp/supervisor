68ee7a4293ee5c60c3fedc09af436f24194d8e8c
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-12-08 00:32:29 +0100

    Adds initial XmlRpc Client

---
+++ src/XmlRpc/Client.php
@@ -0,0 +1,162 @@
+<?php
+
+/*
+ * This file is part of the Indigo Supervisor package.
+ *
+ * (c) Indigo Development Team
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Indigo\Supervisor\XmlRpc;
+
+use Indigo\Http\Client as HttpClient;
+use fXmlRpc\ClientInterface;
+use fXmlRpc\Parser\ParserInterface;
+use fXmlRpc\Parser\XmlReaderParser;
+use fXmlRpc\Serializer\SerializerInterface;
+use fXmlRpc\Serializer\XmlWriterSerializer;
+use fXmlRpc\Exception\ResponseException;
+use fXmlRpc\Exception\InvalidArgumentException;
+
+final class Client implements ClientInterface
+{
+    /**
+     * @var string
+     */
+    private $uri;
+
+    /**
+     * @var HttpClient
+     */
+    private $client;
+
+    /**
+     * @var ParserInterface
+     */
+    private $parser;
+
+    /**
+     * @var SerializerInterface
+     */
+    private $serializer;
+
+    /**
+     * @var array
+     */
+    private $prependParams = [];
+
+    /**
+     * @var array
+     */
+    private $appendParams = [];
+
+    /**
+     * If no specific transport, parser or serializer is passed, default implementations
+     * are used.
+     *
+     * @param string              $uri
+     * @param HttpClient          $transport
+     * @param ParserInterface     $parser
+     * @param SerializerInterface $serializer
+     */
+    public function __construct(
+        HttpClient $client,
+        $uri = null,
+        ParserInterface $parser = null,
+        SerializerInterface $serializer = null
+    )
+    {
+        $this->uri = $uri;
+        $this->client = $client;
+        $this->parser = $parser ?: new XmlReaderParser();
+        $this->serializer = $serializer ?: new XmlWriterSerializer();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getUri()
+    {
+        return $this->uri;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setUri($uri)
+    {
+        if (!is_string($uri)) {
+            throw InvalidArgumentException::expectedParameter(0, 'string', $uri);
+        }
+
+        $this->uri = $uri;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function prependParams(array $params)
+    {
+        $this->prependParams = $params;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getPrependParams()
+    {
+        return $this->prependParams;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function appendParams(array $params)
+    {
+        $this->appendParams = $params;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAppendParams()
+    {
+        return $this->appendParams;
+    }
+
+    /**
+     * {@inheritdoc}
+     * @throws Exception\ResponseException
+     */
+    public function call($methodName, array $params = [])
+    {
+        if (!is_string($methodName)) {
+            throw InvalidArgumentException::expectedParameter(0, 'string', $methodName);
+        }
+
+        $params = array_merge($this->prependParams, $params, $this->appendParams);
+
+        $response = $this->parser->parse(
+            $this->client->get($this->uri, [
+                'body' => $this->serializer->serialize($methodName, $params)
+            ]),
+            $isFault
+        );
+
+        if ($isFault) {
+            throw ResponseException::fault($response);
+        }
+
+        return $response;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function multicall()
+    {
+        return new Multicall($this);
+    }
+}
