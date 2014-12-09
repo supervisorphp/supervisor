b7333c61a0185653a4241f52c144daed441599db
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-12-09 07:25:26 +0100

    Fault should be static

--- RoboFile.php
+++ RoboFile.php
@@ -13,18 +13,12 @@ class RoboFile extends \Robo\Tasks
     public function faultsGenerate()
     {
         $faults = include __DIR__.'/resources/faults.php';
-        $maxStringLength = max(array_map('strlen', $faults));
-        $maxCodeLength = max(array_map('strlen', array_keys($faults)));
 
         $this->taskCleanDir([__DIR__.'/src/Exception/Fault'])->run();
-        $fault = $this->taskWriteToFile(__DIR__.'/src/Exception/Fault.php')->textFromFile(__DIR__.'/resources/Fault.php');
-        $constants = $exceptionMap = [];
 
         foreach ($faults as $code => $name) {
             $exName = $this->createExceptionName($name);
             $file = sprintf(__DIR__.'/src/Exception/Fault/%s.php', $exName);
-            $constants[] = sprintf('    const %s = %d;', $name.str_repeat(' ', $maxStringLength - strlen($name)), $code);
-            $exceptionMap[] = sprintf('        %s => \'Indigo\Supervisor\Exception\Fault\%s\',', $code.str_repeat(' ', $maxCodeLength - strlen($code)), $exName);
 
             $this->taskWriteToFile($file)
                 ->textFromFile(__DIR__.'/resources/FaultTemplate.php')
@@ -32,10 +26,6 @@ class RoboFile extends \Robo\Tasks
                 ->place('FaultName', $exName)
                 ->run();
         }
-
-        $fault->place('CONSTANTS', implode("\n", $constants));
-        $fault->place('EXCEPTION_MAP', implode("\n", $exceptionMap));
-        $fault->run();
     }
 
     /**
